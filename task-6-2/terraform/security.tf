4ea9d2a11fc5b7a903c366cd5678c511c121f68a
minchenlee <110967458+minchenlee@users.noreply.github.com>
2024-04-17 11:28:16 +0800

    Modify: update terraform infra

--- task-6-2/terraform/security.tf
+++ task-6-2/terraform/security.tf
@@ -1,36 +1,9 @@
 ################################################
-# Key pair generation
-################################################
-# Key for admin to access ec2
-resource "tls_private_key" "admin-ec2-ssh-key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-# Create AWS key pair 
-resource "aws_key_pair" "admin-ec2-ssh-key" {
-  key_name   = "thesis-backend-instance-key"
-  public_key = tls_private_key.admin-ec2-ssh-key.public_key_openssh
-  tags = {
-    Belong    = var.belong_tag
-    CreatedBy = "Terraform"
-  }
-}
-
-# Save admin-ec2-ssh-key to local file
-resource "local_file" "private_key" {
-  content         = tls_private_key.admin-ec2-ssh-key.private_key_pem
-  filename        = "${path.module}/thesis-backend-instance-key.pem"
-  file_permission = 0400
-}
-
-
-################################################
 # Security group
 ################################################
 
-# Security group for thesis backend instance
-resource "aws_security_group" "thesis-backend-instance" {
+# Security group for backend instance
+resource "aws_security_group" "backend-instance" {
   name        = "thesis-backend-instance"
   description = "Security group for thesis backend instance"
   vpc_id      = module.thesis-vpc.vpc_id
