4b832576313507123d2ae56d74d512d38a4effe9
minchenlee <110967458+minchenlee@users.noreply.github.com>
2024-04-17 15:41:48 +0800

    Fix: update security.tf

--- task-6-2/terraform/security.tf
+++ task-6-2/terraform/security.tf
@@ -21,7 +21,7 @@ resource "aws_security_group" "backend-instance" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0,0,0,0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
